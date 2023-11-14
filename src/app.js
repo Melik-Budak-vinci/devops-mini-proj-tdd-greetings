1a104b0dcab135de6f892f6cf39f561bcf59f775
hakim-layach-vinci <hakim.layach@student.vinci.be>
2023-11-14 17:24:06 +0100

    add:handle the mix of normal and uppercase names

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
+const { boolean } = require('yargs')
+
 function greet(names) {
-    if(Array.isArray(names)){
-        if(names.length===2)return "Hello, "+names[0]+" and "+names[1]+".";
-        let str='Hello'
+    if (Array.isArray(names)) {
+        if (names.length === 2)
+            return 'Hello, ' + names[0] + ' and ' + names[1] + '.'
+        let str = 'Hello'
+        let hello = ' AND HELLO '
+        let up = false
         for (let index = 0; index < names.length; index++) {
-            if(index===names.length-1){
-                return str+" and "+names[index]+'.'
+            if (index === names.length - 1) {
+                if (up) return str + ' and ' + names[index] + '.' + hello + '!'
+                return str + ' and ' + names[index] + '.'
+            } else if (names[index].toUpperCase() === names[index]) {
+                hello += names[index]
+                up = true
+            } else {
+                str += ', ' + names[index]
             }
-            str+= ", "+names[index];   
         }
     }
-    if (!names || names.trim() === '') return 'Hello, my friend.';
+    if (!names || names.trim() === '') return 'Hello, my friend.'
     if (names === names.toUpperCase()) return 'HELLO, ' + names + '!'
 
     return 'Hello, ' + names + '.'
--- src/app.test.js
+++ src/app.test.js
@@ -1,32 +1,36 @@
 const greet = require('./app')
 
 test('should to be return Hello, (name in table)', () => {
-  const result = greet('Jean-Kevin')
-  expect(result).toEqual("Hello, Jean-Kevin.")
+    const result = greet('Jean-Kevin')
+    expect(result).toEqual('Hello, Jean-Kevin.')
 }),
-test('should to be return Hello, my friend when array is null', () => {
-  const result = greet(null)
-  expect(result).toEqual("Hello, my friend.")
-}),
-test('should to be return Hello, my friend when array is undefind ', () => {
-  const result = greet(undefined)
-  expect(result).toEqual("Hello, my friend.")
-}),
-test('should to be return Hello, my friend when the string in array is empty ', () => {
-  const result = greet("     ")
-  expect(result).toEqual("Hello, my friend.")
-}),
-test('should to be return HELLO, NAME when the name on upper ', () => {
-  const result = greet("KENOBI")
-  expect(result).toEqual("HELLO, KENOBI!")
-}),
-test('should to be return Hello, name and second name when we have two name in array ',()=>{
-  const result = greet(["Kratos","Thanathos"])
+    test('should to be return Hello, my friend when array is null', () => {
+        const result = greet(null)
+        expect(result).toEqual('Hello, my friend.')
+    }),
+    test('should to be return Hello, my friend when array is undefind ', () => {
+        const result = greet(undefined)
+        expect(result).toEqual('Hello, my friend.')
+    }),
+    test('should to be return Hello, my friend when the string in array is empty ', () => {
+        const result = greet('     ')
+        expect(result).toEqual('Hello, my friend.')
+    }),
+    test('should to be return HELLO, NAME when the name on upper ', () => {
+        const result = greet('KENOBI')
+        expect(result).toEqual('HELLO, KENOBI!')
+    }),
+    test('should to be return Hello, name and second name when we have two name in array ', () => {
+        const result = greet(['Kratos', 'Thanathos'])
 
-  expect(result).toEqual("Hello, Kratos and Thanathos.");
-}),
-test('should to be return Hello, name and second name when we have more than two name in array ',()=>{
-  const result = greet(["Kratos","Thanathos","Hypnos"])
+        expect(result).toEqual('Hello, Kratos and Thanathos.')
+    }),
+    test('should to be return Hello, name and second name when we have more than two name in array ', () => {
+        const result = greet(['Kratos', 'Thanathos', 'Hypnos'])
 
-  expect(result).toEqual("Hello, Kratos, Thanathos and Hypnos.");
+        expect(result).toEqual('Hello, Kratos, Thanathos and Hypnos.')
+    })
+test('should to be return Hello, name on uppercase and other name when we have one name on upper in this array', () => {
+    const result = greet(['KRATOS', 'Thanathos', 'Hypnos'])
+    expect(result).toEqual('Hello, Thanathos and Hypnos. AND HELLO KRATOS!')
 })
