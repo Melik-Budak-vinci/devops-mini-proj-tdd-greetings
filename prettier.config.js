627c18744c0a097ccd35c728944c663e7aac0f95
hakim-layach-vinci <hakim.layach@student.vinci.be>
2023-12-11 20:13:28 +0100

    add:install dependencies in pipeline

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 const config = {
-    trailingComma: 'es5',
+    trailingComma: "es5",
     tabWidth: 4,
     semi: false,
     singleQuote: true,
-}
+};
 
-module.exports = config
+module.exports = config;
--- src/app.js
+++ src/app.js
@@ -1,78 +1,77 @@
-const { boolean } = require('yargs')
 
 function greet(names) {
     if (Array.isArray(names)) {
         const greetInLower = handleArray(
             filterInLower(names),
             names[names.length - 1]
-        )
-        const filterUpper = filterInUpper(names)
+        );
+        const filterUpper = filterInUpper(names);
 
         if (filterUpper.length === 0) {
-            return greetInLower
+            return greetInLower;
         }
-        const greetInUpper = handleArray(filterUpper, names[names.length - 1])
-        return greetInLower + greetInUpper
+        const greetInUpper = handleArray(filterUpper, names[names.length - 1]);
+        return greetInLower + greetInUpper;
     }
-    if (isIllegalArgument(names)) return 'Hello, my friend.'
-    if (isUpper(names)) return 'HELLO, ' + names + '!'
+    if (isIllegalArgument(names)) return "Hello, my friend.";
+    if (isUpper(names)) return "HELLO, " + names + "!";
 
-    return 'Hello, ' + names + '.'
+    return "Hello, " + names + ".";
 }
 
 function filterInLower(names) {
     return names.filter(
-        (name) => !isUpper(name) && name !== 'fr' && name !== 'nl'
-    )
+        (name) => !isUpper(name) && name !== "fr" && name !== "nl"
+    );
 }
 function filterInUpper(names) {
     return names.filter(
-        (name) => isUpper(name) && name !== 'fr' && name !== 'nl'
-    )
+        (name) => isUpper(name) && name !== "fr" && name !== "nl"
+    );
 }
 
 function handleArray(names, language) {
-    let str = 'Hello'
-    let and = ' and '
-    let point = '.'
-    let comma = ', '
-    let isUp = isUpper(names[0])
-    if (language === 'fr') {
-        str = 'Bonjour'
-        and = ' et '
+    let str = "Hello";
+    let and = " and ";
+    let point = ".";
+    let comma = ", ";
+    let isUp = isUpper(names[0]);
+    if (language === "fr") {
+        str = "Bonjour";
+        and = " et ";
     }
-    if (language === 'nl') {
-        str = 'Hallo'
-        and = ' en '
+    if (language === "nl") {
+        str = "Hallo";
+        and = " en ";
     }
     if (isUp) {
-        lowerToUpper()
+        lowerToUpper();
     }
     if (names.length === 1) {
-        return str + comma + names[0] + point
+        return str + comma + names[0] + point;
     }
     for (let index = 0; index < names.length; index++) {
         if (index === names.length - 1) {
-            return str + and + names[index] + point
+            return str + and + names[index] + point;
         } else if (index > 0) {
-            str += ', ' + names[index]
+            str += ", " + names[index];
         } else {
-            str += comma + names[index]
+            str += comma + names[index];
         }
     }
 
     function lowerToUpper() {
-        str = and.toUpperCase() + str.toUpperCase()
-        and = and.toUpperCase()
-        point = '!'
-        comma = ' '
+        str = and.toUpperCase() + str.toUpperCase();
+        and = and.toUpperCase();
+        point = "!";
+        comma = " ";
     }
 }
 function isIllegalArgument(names) {
-    return !names || names.trim() === ''
+    return !names || names.trim() === "";
 }
 function isUpper(names) {
-    return names === names.toUpperCase()
+    return names === names.toUpperCase();
 }
 
-module.exports = greet
+module.exports = greet;
