e230a003b633f73ba36b2cdefa7da82b4209e331
melik <melik.budak@student.vinci.be>
2023-12-11 23:30:18 +0100

    add: setup webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,13 +15,24 @@ module.exports = {
             parserOptions: {
                 sourceType: 'script',
             },
+            rules: {
+                'node/no-unsupported-features/es-builtins': [
+                    'error',
+                    { version: '>=14.0.0' },
+                ], // Règle pour reconnaître __dirname
+                'node/no-unsupported-features/es-syntax': [
+                    'error',
+                    { version: '>=14.0.0' },
+                ], // Règle pour reconnaître __dirname
+                // Vous pouvez ajouter d'autres règles spécifiques à Node.js si nécessaire
+            },
         },
     ],
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['prettier'],
+    plugins: ['prettier', 'node'],
     rules: {
         'prettier/prettier': 'error',
         semi: ['error', 'always'],
